3c48e5dc86e4e18888b2c8de9343ae0d50d151e6
keitlyn-lau <78679013+keitlyn-lau@users.noreply.github.com>
2023-03-01 12:21:36 -0800

    Add files via upload

---
+++ MDIA-2294-[230301]-CW-09/Student.js
@@ -0,0 +1,41 @@
+﻿// Created: 202X-XX-XX
+// Author: YOURNAME
+// Last Updated: 202X-XX-XX
+// Description: Student class including some methods
+class Student {
+
+    constructor(id, fname, lname, dob) {
+        // properties
+        this.student_id = id;
+        this.first_name = fname;
+        this.last_name = lname;
+        this.birthday = dob;
+    }
+    
+    // return the full name
+    full_name () {
+        return this.first_name + " " + this.last_name;
+    }
+
+    // return the current age
+    get_age(){
+
+        const today = new Date();
+        
+        if (today.getMonth() > this.birthday.getMonth()) {
+            // past month
+            return today.getFullYear() - this.birthday.getFullYear();
+        } 
+        else if (today.getMonth() == this.birthday.getMonth() && 
+                 today.getDate() >= this.birthday.getDate()) {
+            // same month but past day
+            return today.getFullYear() - this.birthday.getFullYear();
+        }
+        else{
+            // some future date this year so subtract a year
+            return today.getFullYear() - this.birthday.getFullYear() - 1;
+        }
+    }
+
+}  // end of class Student
+
